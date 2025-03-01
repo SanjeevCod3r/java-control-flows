ada1b50e44b98205b1e1be2035c3ecff6535b725
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-03-01 14:43:19 +0530

    [Sanjeev Jaiswal] ADD: Rewritten program that performs the countdown using a for loop instead of a while loop

---
+++ RocketLaunchTimer.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RocketLaunchTimer {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the countdown start number: ");
+        int counter = input.nextInt();
+
+        for (int i = counter; i >= 1; i--) {
+            System.out.println("Countdown: " + i);
+        }
+
+        System.out.println("Launch!");
+    }
+}
