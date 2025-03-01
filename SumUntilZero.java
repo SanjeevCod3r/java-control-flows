ba22d12cbe9d5e899910460f8eb428587a6db6e0
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-03-01 14:46:01 +0530

    [Sanjeev Jaiswal] ADD: Java program that calculates the sum of numbers until the user enters 0

---
+++ SumUntilZero.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SumUntilZero {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double total = 0.0;
+        double number;
+
+        System.out.println("Enter numbers to sum (enter 0 to stop):");
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            number = input.nextDouble();
+
+            if (number == 0) {
+                break; // Exit the loop if the user enters 0
+            }
+
+            total += number; // Add the entered number to the total
+        }
+
+        System.out.println("The total sum is: " + total);
+       
+    }
+}
